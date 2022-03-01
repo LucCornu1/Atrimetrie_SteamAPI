7c2fcf77721902de56a6fe4ed3f98abff255bfd1
ZeGoblyn <j.ziegeltrum@ludus-academie.com>
2022-03-01 09:02:26 +0100

    Menu selection
    Ecran de selection de rôle sur le lobby

--- Source/Atrimetrie/SteamAPISubsystem.cpp
+++ Source/Atrimetrie/SteamAPISubsystem.cpp
@@ -23,4 +23,79 @@ void USteamAPISubsystem::Initialize(FSubsystemCollectionBase& Collection)
 void USteamAPISubsystem::Deinitialize()
 {
 	SteamAPI_Shutdown();
+<<<<<<< Updated upstream
+=======
+
+}
+
+bool USteamAPISubsystem::Tick(float DeltaTime)
+{
+	SteamAPI_RunCallbacks();
+	return true;
+
+}
+
+
+// Friends
+void USteamAPISubsystem::SetSteamFriendArray()
+{
+	int MaxFriendCount = SteamFriends()->GetFriendCount(k_EFriendFlagImmediate);
+
+	for (int i = 0; i++; i < MaxFriendCount)
+	{
+		SteamFriendArray[i] = SteamFriends()->GetFriendByIndex(i, k_EFriendFlagImmediate);
+	}
+
+}
+
+
+// Lobbies
+void USteamAPISubsystem::OnLobbyEntered(LobbyEnter_t* pCallback)
+{
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Blue, FString::Printf(TEXT("You've entered a Lobby")));
+	}
+
+}
+
+void USteamAPISubsystem::OnLobbyCreated(LobbyCreated_t* pCallback, bool bIOFailure)
+{
+	if (pCallback->m_eResult != k_EResultOK || bIOFailure)
+	{
+		UE_LOG(LogTemp, Error, TEXT("Lobby creation failed"));
+		return;
+	}
+
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Red, FString::Printf(TEXT("You've created a Lobby %d"), pCallback->m_ulSteamIDLobby));
+	}
+
+}
+
+void USteamAPISubsystem::InviteFriend(CSteamID UserId)
+{
+	if (SteamMatchmaking()->InviteUserToLobby(LobbyId, UserId))
+	{
+		if (GEngine)
+		{
+			GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Red, FString::Printf(TEXT("Friend invited")));
+		}
+	}
+
+}
+
+void USteamAPISubsystem::JoinFriendLobby()
+{
+	// None
+
+}
+
+void USteamAPISubsystem::CreateFriendLobby_Implementation(int nMaxMembers)
+{
+	SteamAPICall_t hSteamAPICall = SteamMatchmaking()->CreateLobby(k_ELobbyTypeFriendsOnly, nMaxMembers);
+	m_LobbyCreatedCallResult.Set(hSteamAPICall, this, &USteamAPISubsystem::OnLobbyCreated);
+
+>>>>>>> Stashed changes
 }
--- Source/Atrimetrie/SteamAPISubsystem.h
+++ Source/Atrimetrie/SteamAPISubsystem.h
@@ -25,10 +25,50 @@ class ATRIMETRIE_API USteamAPISubsystem : public UGameInstanceSubsystem
 protected:
 	static constexpr char* APP_ID = RAW_APP_ID;
 	CSteamID MyId;
+<<<<<<< Updated upstream
 		
+=======
+	CSteamID LobbyId;
+
+	FDelegateHandle TickDelegateHandle;
+	
+>>>>>>> Stashed changes
 public:
 	// Begin USubsystem
 	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
 	virtual void Deinitialize() override;
 	// End USubsystem
+<<<<<<< Updated upstream
+=======
+
+	// Called every frame
+	bool Tick(float DeltaTime);
+
+
+// Friends
+protected:
+	CSteamID SteamFriendArray[100];
+
+protected:
+	UFUNCTION(BlueprintCallable, Category = "SteamAPI|Friends")
+		void SetSteamFriendArray();
+
+
+// Lobbies
+private:
+	STEAM_CALLBACK(USteamAPISubsystem, OnLobbyEntered, LobbyEnter_t);
+
+	void OnLobbyCreated(LobbyCreated_t* pCallback, bool bIOFailure);
+	CCallResult<USteamAPISubsystem, LobbyCreated_t> m_LobbyCreatedCallResult;
+
+public:
+	UFUNCTION(BlueprintCallable, Server, Reliable, Category = "SteamAPI|Lobby")
+		void CreateFriendLobby(int nMaxMembers);
+
+	UFUNCTION(BlueprintCallable, Category = "SteamAPI|Lobby")
+		void InviteFriend(CSteamID UserId);
+
+	UFUNCTION(BlueprintCallable, Category = "SteamAPI|Lobby")
+		void JoinFriendLobby();
+>>>>>>> Stashed changes
 };
